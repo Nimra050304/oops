1cc3b6df9284b86907a5ea70f24273f4c0bf0bb7
Nimra050304 <arminpcemail1@gmail.com>
2025-09-14 15:07:03 +0530

    Create 6_calc_by_switch.c

---
+++ 6_calc_by_switch.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main() {
+
+    
+    int input1,input2;
+    printf("Input the value of first number \n");
+    scanf("%d",&input1);
+
+    char operator;
+    printf("Enter the operator. Enter 't' or 'T' for termination\n");
+    scanf(" %c",&operator);
+
+    float ans=input1;
+
+    
+    do
+    {
+
+        printf("Input the value of second input\n");
+        scanf("%d",&input2);
+        
+        switch(operator)
+        {
+            case '+':
+                ans+=input2;
+                break;
+            case '-':
+                ans-=input2;
+                break;
+            case '*':
+                ans*=input2;
+                break;
+            case'/':
+                ans/=input2;
+                break;
+            default:
+                printf("Invalid operator. Try again");
+                break;
+
+        }
+
+        printf("Enter the operator. Enter 't' or 'T' for termination\n");
+        scanf(" %c",&operator);
+
+    }while(operator!='t'&&operator!='T');
+
+    printf("The final answer is %f",ans);
+
+    return 0;
+}
